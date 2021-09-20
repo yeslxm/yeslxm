63e4d28f60b5a61696eab851cb44bb1f9338bfd0
yeslxm <91017697+yeslxm@users.noreply.github.com>
2021-09-20 09:30:39 +0800

    Add files via upload

---
+++ png/Mini world 1.lua
@@ -0,0 +1,11 @@
+while( true )
+do
+World:spawnCreature(9, 9, 9, 3122)
+World:spawnCreature(9, 9, 9, 3121)
+World:spawnCreature(9, 9, 9, 3120)
+World:spawnCreature(9, 9, 9, 3123)
+World:spawnCreature(9, 9, 9, 3124)
+World:spawnCreature(9, 9, 9, 3126)
+end
+Game:msgBox("创建成功")
+Chat:sendSystemMsg("生物创建成功9.9.9")
